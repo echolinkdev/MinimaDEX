11548a74563a37531c32545d9555815723017279
Echolink <echolinkdev3@gmail.com>
2026-04-17 17:56:52 +0100

    Update dexconfig.js to add Jinbe Coin market with fixed 2.5 Minima to 1 Jinbe rate

---
+++ dexconfig.js
@@ -0,0 +1,10 @@
+// Adding Jinbe Coin market with fixed rates
+const markets = [
+    // Existing markets
+    {
+        name: 'Jinbe Coin',
+        rate: 2.5,
+        currency: 'Minima',
+    },
+    // Other markets
+];
